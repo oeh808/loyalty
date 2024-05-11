923a2a56ad506e870387b532b3424dc9c3701647
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-11 19:33:11 +0300

    First commit

---
+++ src/main/java/io/brightskies/loyalty/LoyaltyApplication.java
@@ -0,0 +1,13 @@
+package io.brightskies.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyApplication.class, args);
+	}
+
+}
