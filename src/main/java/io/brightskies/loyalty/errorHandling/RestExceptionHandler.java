6113d208e86d9b837afc51a8d6f362b9d0501993
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-12 14:24:02 +0300

    Updated error handler to extend ResponseEntityExceptionHandler

--- src/main/java/io/brightskies/loyalty/errorHandling/RestExceptionHandler.java
+++ src/main/java/io/brightskies/loyalty/errorHandling/RestExceptionHandler.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import io.brightskies.loyalty.pointsEntry.exception.PointsEntryException;
 import io.brightskies.loyalty.product.exception.ProductException;
 
 @ControllerAdvice
-public class RestExceptionHandler {
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
