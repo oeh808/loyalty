2702695fe19322a5ed875429e6fd3fec60a40f84
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-12 10:39:36 +0300

    Changed the primary key that the customer entity uses to phoneNumber

--- src/main/java/io/brightskies/loyalty/customer/entity/Customer.java
+++ src/main/java/io/brightskies/loyalty/customer/entity/Customer.java
@@ -1,9 +1,6 @@
 package io.brightskies.loyalty.customer.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -17,9 +14,6 @@ import lombok.NoArgsConstructor;
 @Table(name = "Customers")
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @Column(unique = true)
     private String phoneNumber;
     private int totalPoints;
 }
