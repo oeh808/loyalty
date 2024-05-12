4e1b807f975609d1cc48bd4a5b251af61533dadc
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-12 13:38:40 +0300

    Added further verification for product update test

--- src/test/java/io/brightskies/loyalty/service/ProductServiceTest.java
+++ src/test/java/io/brightskies/loyalty/service/ProductServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -118,6 +119,8 @@ public class ProductServiceTest {
                 });
 
         assertTrue(ex.getMessage().contains(ProductExceptionMessages.PRODUCT_NOT_FOUND));
+
+        verify(productRepo, times(0)).save(any(Product.class));
     }
 
     @Test
