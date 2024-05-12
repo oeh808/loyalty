6fc8f1f74319e88804de27f5b3f152f51d74cbca
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-12 09:40:56 +0300

    Updated relation with product

--- src/main/java/io/brightskies/loyalty/order/ProductsOrdered.java
+++ src/main/java/io/brightskies/loyalty/order/ProductsOrdered.java
@@ -2,6 +2,9 @@ package io.brightskies.loyalty.order;
 
 import io.brightskies.loyalty.product.entity.Product;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +14,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Embeddable
 public class ProductsOrdered {
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productId")
     private Product product;
     private int quantity;
 }
