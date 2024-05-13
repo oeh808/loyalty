898af634eabad787eccfe98d3afccf51b13831e7
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-13 15:33:39 +0300

    Removed unused import

--- src/main/java/io/brightskies/loyalty/user/service/UserService.java
+++ src/main/java/io/brightskies/loyalty/user/service/UserService.java
@@ -1,19 +1,15 @@
 package io.brightskies.loyalty.user.service;
 
-
 import io.brightskies.loyalty.user.DTO.UserDTO;
-import io.brightskies.loyalty.user.Entities.Role;
 import io.brightskies.loyalty.user.Entities.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
 
 public interface UserService extends UserDetailsService {
     User findByEmail(String email);
 
     User findById(Long id);
-    User addUser(UserDTO user);
 
+    User addUser(UserDTO user);
 
     void attachRoleToUser(String userName, String roleName);
 }
-
