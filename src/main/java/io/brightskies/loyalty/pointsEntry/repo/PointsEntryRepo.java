e778508b1bddc3761d781694d5424dacaa691a13
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-13 11:57:14 +0300

    Implemented method to return a sorted list of point entries of a given customer

--- src/main/java/io/brightskies/loyalty/pointsEntry/repo/PointsEntryRepo.java
+++ src/main/java/io/brightskies/loyalty/pointsEntry/repo/PointsEntryRepo.java
@@ -3,7 +3,10 @@ package io.brightskies.loyalty.pointsEntry.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.brightskies.loyalty.pointsEntry.entity.PointsEntry;
+import java.util.List;
+import io.brightskies.loyalty.customer.entity.Customer;
+
 
 public interface PointsEntryRepo extends JpaRepository<PointsEntry, Long> {
-
+    List<PointsEntry> findByCustomerOrderByExpiryDateAsc(Customer customer);
 }
---
+++ src/test/java/io/brightskies/loyalty/repo/PointsEntryRepoTest.java
@@ -0,0 +1,77 @@
+package io.brightskies.loyalty.repo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import io.brightskies.loyalty.customer.entity.Customer;
+import io.brightskies.loyalty.customer.repo.CustomerRepo;
+import io.brightskies.loyalty.pointsEntry.entity.PointsEntry;
+import io.brightskies.loyalty.pointsEntry.repo.PointsEntryRepo;
+
+@ActiveProfiles("test")
+@DataJpaTest
+public class PointsEntryRepoTest {
+    @Autowired
+    private CustomerRepo customerRepo;
+
+    @Autowired
+    private PointsEntryRepo pointsEntryRepo;
+
+    private static Customer customer;
+
+    private static PointsEntry pointsEntry1;
+    private static PointsEntry pointsEntry2;
+    private static PointsEntry pointsEntry3;
+
+    @BeforeAll
+    public static void setUp() {
+        customer = new Customer(0, "0291303192", 200);
+
+        // Expiration dates hierarchy from first to expire to last to expire:
+        // pointsEntry1 -> pointsEntry3 -> pointsEntry2
+        pointsEntry1 = new PointsEntry(0, 150, Date.valueOf("2030-02-17"), null);
+        pointsEntry2 = new PointsEntry(0, 50, Date.valueOf("2030-04-17"), null);
+        pointsEntry3 = new PointsEntry(0, 50, Date.valueOf("2030-03-17"), null);
+    }
+
+    @BeforeEach
+    public void setUpForEach() {
+        customer = customerRepo.save(customer);
+
+        pointsEntry1.setCustomer(customer);
+        pointsEntryRepo.save(pointsEntry1);
+
+        pointsEntry2.setCustomer(customer);
+        pointsEntryRepo.save(pointsEntry2);
+
+        pointsEntry3.setCustomer(customer);
+        pointsEntryRepo.save(pointsEntry3);
+    }
+
+    @AfterEach
+    public void tearDownForEach() {
+        pointsEntryRepo.deleteAll();
+        customerRepo.deleteAll();
+    }
+
+    @Test
+    public void findByCustomerOrderByExpiryDateAsc_ReturnsListOfPointEntriesInAscendingOrder() {
+        List<PointsEntry> sortedPointsEntries = new ArrayList<>();
+        sortedPointsEntries.add(pointsEntry1);
+        sortedPointsEntries.add(pointsEntry3);
+        sortedPointsEntries.add(pointsEntry2);
+
+        assertEquals(sortedPointsEntries, pointsEntryRepo.findByCustomerOrderByExpiryDateAsc(customer));
+    }
+}
