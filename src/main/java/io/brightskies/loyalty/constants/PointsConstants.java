15c351b82643d606a912456a1c4cd8a5227f7dc7
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-13 14:28:13 +0300

    Added configurable constant for converting points into money

--- src/main/java/io/brightskies/loyalty/constants/PointsConstants.java
+++ src/main/java/io/brightskies/loyalty/constants/PointsConstants.java
@@ -5,4 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 public class PointsConstants {
     @Value("${monthsUntilPointsExpire:2}")
     public static int MONTHS_UNTIL_EXPIRY;
+
+    @Value("${moneyEquivalenceToOnePoint:0.25f}")
+    public static float WORTH_OF_ONE_POINT;
 }
