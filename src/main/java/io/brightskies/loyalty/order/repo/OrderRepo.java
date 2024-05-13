a20b0cb3392fe838133758e1753d92cc0093340a
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-13 10:50:33 +0300

    Added method for getting orders given a customer's phone number

--- src/main/java/io/brightskies/loyalty/order/repo/OrderRepo.java
+++ src/main/java/io/brightskies/loyalty/order/repo/OrderRepo.java
@@ -3,7 +3,8 @@ package io.brightskies.loyalty.order.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.brightskies.loyalty.order.entity.Order;
+import java.util.List;
 
 public interface OrderRepo extends JpaRepository<Order, Long> {
-
+    List<Order> findByCustomer_PhoneNumber(String phoneNumber);
 }
