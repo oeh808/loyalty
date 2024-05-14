391cae38a16d2d8637f5947c44d86c60eb993e1a
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-14 14:20:29 +0300

    Created a configuration file for the API

---
+++ src/main/java/io/brightskies/loyalty/openApi/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package io.brightskies.loyalty.openApi;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Loyalty", description = "OpenAPI documentation for loyalty points project.", version = "1.0"))
+public class OpenApiConfig {
+
+}
