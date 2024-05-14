167efa0a409c248134c411cc5044f4e8990e628a
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-14 16:26:39 +0300

    Updated swagger annotation for Order Controller

--- src/main/java/io/brightskies/loyalty/order/controller/OrderController.java
+++ src/main/java/io/brightskies/loyalty/order/controller/OrderController.java
@@ -6,7 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 import io.brightskies.loyalty.order.dtos.OrderCreationDto;
 import io.brightskies.loyalty.order.entity.Order;
 import io.brightskies.loyalty.order.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -22,28 +27,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class OrderController {
     private OrderService orderService;
 
+    @Operation(description = "POST endpoint for creating an order" +
+            "\n\n Returns the order created.", summary = "Create an Order")
     @PostMapping()
-    public Order placeOrder(@RequestBody OrderCreationDto dto) {
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of OrderCreationDto")
+    public Order placeOrder(@Valid @RequestBody OrderCreationDto dto) {
         return orderService.placeOrder(dto.orderedProducts(), dto.moneySpent(), dto.pointsSpent(), dto.phoneNumber());
     }
 
+    @Operation(description = "GET endpoint for retrieving a single order given its id" +
+            "\n\n Returns an instance of type order.", summary = "Get Single Order")
     @GetMapping("/{id}")
-    public Order getSingleOrder(@PathVariable long id) {
+    public Order getSingleOrder(
+            @Parameter(in = ParameterIn.PATH, name = "id", description = "Order ID") @PathVariable long id) {
         return orderService.getOrder(id);
     }
 
+    @Operation(description = "GET endpoint for retrieving ALL orders" +
+            "\n\n Returns a list of orders.", summary = "Get All Orders")
     @GetMapping()
     public List<Order> getAllOrders() {
         return orderService.getAllOrders();
     }
 
+    @Operation(description = "GET endpoint for retrieving all orders associated with a customer's phone number" +
+            "\n\n Returns a list of orders.", summary = "Get Customer's Orders")
     @GetMapping("/customerOrders/{phoneNumber}")
-    public List<Order> getAllOrdersFromCustomer(@PathVariable String phoneNumber) {
+    public List<Order> getAllOrdersFromCustomer(
+            @Parameter(in = ParameterIn.PATH, name = "phoneNumber", description = "Phone Number") @PathVariable String phoneNumber) {
         return orderService.getOrdersByCustomer(phoneNumber);
     }
 
+    @Operation(description = "DELETE endpoint for deleting a single order given its id" +
+            "\n\n Returns a String confirming the delete was succcessful.", summary = "Delete Order")
     @DeleteMapping("/{id}")
-    public String deleteOrder(@PathVariable long id) {
+    public String deleteOrder(
+            @Parameter(in = ParameterIn.PATH, name = "id", description = "Order ID") @PathVariable long id) {
         orderService.deleteOrder(id);
         return "Order deleted successfully";
     }
