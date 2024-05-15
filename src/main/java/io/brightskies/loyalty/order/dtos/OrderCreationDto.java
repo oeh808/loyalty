268c21e4d2f8b4296dd3f5b0213f3de62cd7c295
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-15 11:35:51 +0300

    Fixed typo in error message that should be shown for invalid list of products ordered

--- src/main/java/io/brightskies/loyalty/order/dtos/OrderCreationDto.java
+++ src/main/java/io/brightskies/loyalty/order/dtos/OrderCreationDto.java
@@ -11,14 +11,14 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record OrderCreationDto(
-        @Schema(requiredProperties = {
-                ValidationMessages.NOT_BLANK_SCHEMA }) @NotNull(message = "orderedProducts"
-                        + ValidationMessages.NOT_BLANK) List<OrderedProduct> orderedProducts,
-        @Schema(minimum = "0") @PositiveOrZero(message = "moneySpent"
-                + ValidationMessages.POSITIVE_OR_ZERO) float moneySpent,
-        @Schema(minimum = "0") @PositiveOrZero(message = "pointsSpent"
-                + ValidationMessages.POSITIVE_OR_ZERO) int pointsSpent,
-        @Schema(requiredProperties = {
-                ValidationMessages.NOT_BLANK_SCHEMA }) @NotBlank(message = "phone number"
-                        + ValidationMessages.NOT_BLANK) @Pattern(message = ValidationMessages.INVALID_PHONE_NUMBER, regexp = ValidationMessages.PHONE_NUMBER_REGEX) String phoneNumber){
+                @Schema(requiredProperties = {
+                                ValidationMessages.NOT_NULL_SCHEMA }) @NotNull(message = "orderedProducts"
+                                                + ValidationMessages.NOT_NULL) List<OrderedProduct> orderedProducts,
+                @Schema(minimum = "0") @PositiveOrZero(message = "moneySpent"
+                                + ValidationMessages.POSITIVE_OR_ZERO) float moneySpent,
+                @Schema(minimum = "0") @PositiveOrZero(message = "pointsSpent"
+                                + ValidationMessages.POSITIVE_OR_ZERO) int pointsSpent,
+                @Schema(requiredProperties = {
+                                ValidationMessages.NOT_BLANK_SCHEMA }) @NotBlank(message = "phone number"
+                                                + ValidationMessages.NOT_BLANK) @Pattern(message = ValidationMessages.INVALID_PHONE_NUMBER, regexp = ValidationMessages.PHONE_NUMBER_REGEX) String phoneNumber){
 }
