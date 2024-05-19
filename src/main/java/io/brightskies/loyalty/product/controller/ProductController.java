c9cdefc31ac1466904b3679309bf9beeb3ae6fc4
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-19 12:56:36 +0300

    Fixed typo with getAllProducts

--- src/main/java/io/brightskies/loyalty/product/controller/ProductController.java
+++ src/main/java/io/brightskies/loyalty/product/controller/ProductController.java
@@ -28,59 +28,58 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Tag(name = "Products", description = "Controller for handling mappings for products")
 @RequestMapping("/products")
 public class ProductController {
-    private ProductService productService;
-    private ProductMapper productMapper;
+        private ProductService productService;
+        private ProductMapper productMapper;
 
-    public ProductController(ProductService productService, ProductMapper productMapper) {
-        this.productService = productService;
-        this.productMapper = productMapper;
-    }
+        public ProductController(ProductService productService, ProductMapper productMapper) {
+                this.productService = productService;
+                this.productMapper = productMapper;
+        }
 
-    @Operation(description = "POST endpoint for creating a product" +
-            "\n\n Returns the product created.", summary = "Create a product")
-    @PostMapping()
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of ProductCreationDto")
-    public Product createProduct(@Valid @RequestBody ProductCreationDto dto) {
-        Product product = productMapper.toProduct(dto);
-        return productService.createProduct(product);
-    }
+        @Operation(description = "POST endpoint for creating a product" +
+                        "\n\n Returns the product created.", summary = "Create a product")
+        @PostMapping()
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of ProductCreationDto")
+        public Product createProduct(@Valid @RequestBody ProductCreationDto dto) {
+                Product product = productMapper.toProduct(dto);
+                return productService.createProduct(product);
+        }
 
-    @Operation(description = "GET endpoint for retrieving a single product given its id" +
-            "\n\n Returns an instance of type product.", summary = "Get Single Product")
-    @GetMapping("/{id}")
-    public Product getProduct(
-            @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id) {
-        return productService.getProduct(id);
-    }
+        @Operation(description = "GET endpoint for retrieving a single product given its id" +
+                        "\n\n Returns an instance of type product.", summary = "Get Single Product")
+        @GetMapping("/{id}")
+        public Product getProduct(
+                        @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id) {
+                return productService.getProduct(id);
+        }
 
-    @Operation(description = "GET endpoint for retrieving ALL products" +
-            "\n\n Returns a list of products.", summary = "Get All Products")
-    @GetMapping()
-    public List<Product> getAllProducts(
-            @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id) {
-        return productService.getAllProducts();
-    }
+        @Operation(description = "GET endpoint for retrieving ALL products" +
+                        "\n\n Returns a list of products.", summary = "Get All Products")
+        @GetMapping()
+        public List<Product> getAllProducts() {
+                return productService.getAllProducts();
+        }
 
-    @Operation(description = "PUT endpoint for updating a product" +
-            "\n\n Returns the product updated.", summary = "Update a product")
-    @PutMapping("/{id}")
-    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Body is of the type ProductUpdatingDto."
-            + "\n \n All fields are optional."
-            + " Fields that are left blank are filled by equivalent fields from the original product.")
-    public Product updateProduct(
-            @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id,
-            @RequestBody ProductUpdatingDto dto) {
-        Product product = productMapper.toProduct(dto);
-        return productService.updateProduct(id, product);
-    }
+        @Operation(description = "PUT endpoint for updating a product" +
+                        "\n\n Returns the product updated.", summary = "Update a product")
+        @PutMapping("/{id}")
+        @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Body is of the type ProductUpdatingDto."
+                        + "\n \n All fields are optional."
+                        + " Fields that are left blank are filled by equivalent fields from the original product.")
+        public Product updateProduct(
+                        @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id,
+                        @RequestBody ProductUpdatingDto dto) {
+                Product product = productMapper.toProduct(dto);
+                return productService.updateProduct(id, product);
+        }
 
-    @Operation(description = "DELETE endpoint for deleting a single product given its id" +
-            "\n\n Returns a String confirming the delete was succcessful.", summary = "Delete Product")
-    @DeleteMapping("/{id}")
-    public String deleteProduct(
-            @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id) {
-        productService.deleteProduct(id);
-        return "Product deleted successfully";
-    }
+        @Operation(description = "DELETE endpoint for deleting a single product given its id" +
+                        "\n\n Returns a String confirming the delete was succcessful.", summary = "Delete Product")
+        @DeleteMapping("/{id}")
+        public String deleteProduct(
+                        @Parameter(in = ParameterIn.PATH, name = "id", description = "Product ID") @PathVariable long id) {
+                productService.deleteProduct(id);
+                return "Product deleted successfully";
+        }
 
 }
