5a16fa4fd5f685ba0e8df32be4b03f5ce08ad33e
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-19 16:40:26 +0300

    Temporarily gave static constants fixed values until @Value issue is solved

--- src/main/java/io/brightskies/loyalty/constants/PointsConstants.java
+++ src/main/java/io/brightskies/loyalty/constants/PointsConstants.java
@@ -2,10 +2,11 @@ package io.brightskies.loyalty.constants;
 
 import org.springframework.beans.factory.annotation.Value;
 
+// FIXME: Sort out @Value
 public class PointsConstants {
     @Value("${monthsUntilPointsExpire:2}")
-    public static int MONTHS_UNTIL_EXPIRY;
+    public static int MONTHS_UNTIL_EXPIRY = 2;
 
     @Value("${moneyEquivalenceToOnePoint:0.25f}")
-    public static float WORTH_OF_ONE_POINT;
+    public static float WORTH_OF_ONE_POINT = 0.25f;
 }
