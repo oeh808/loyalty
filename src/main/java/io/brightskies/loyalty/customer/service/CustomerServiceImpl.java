50ed51e9fe00ee575894b4cc6e6b5e57f1d9072d
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-20 16:22:58 +0300

    Implemented logging for customer service

--- src/main/java/io/brightskies/loyalty/customer/service/CustomerServiceImpl.java
+++ src/main/java/io/brightskies/loyalty/customer/service/CustomerServiceImpl.java
@@ -10,8 +10,12 @@ import io.brightskies.loyalty.customer.exception.CustomerException;
 import io.brightskies.loyalty.customer.exception.CustomerExceptionMessages;
 import io.brightskies.loyalty.customer.repo.CustomerRepo;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @Service
 public class CustomerServiceImpl implements CustomerService {
+
     private CustomerRepo customerRepo;
 
     public CustomerServiceImpl(CustomerRepo customerRepo) {
@@ -20,67 +24,87 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer createCustomer(Customer customer) {
+        log.info("Running createCustomer(" + customer.toString() + ") in CustomerServiceImpl...");
         return customerRepo.save(customer);
     }
 
     @Override
     public Customer getCustomer(long id) {
+        log.info("Running getCustomer(" + id + ") in CustomerServiceImpl...");
         Optional<Customer> opCustomer = customerRepo.findById(id);
         if (opCustomer.isPresent()) {
             return opCustomer.get();
         } else {
+            log.error("Invalid customer id: " + id + "!");
             throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_ID);
         }
     }
 
     @Override
     public Customer getCustomer(String phoneNumber) {
+        log.info("Running getCustomer(" + phoneNumber + ") in CustomerServiceImpl...");
         Optional<Customer> opCustomer = customerRepo.findByPhoneNumber(phoneNumber);
         if (opCustomer.isPresent()) {
             return opCustomer.get();
         } else {
+            log.error("Invalid customer phone number: " + phoneNumber + "!");
             throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_PHONE_NUMBER);
         }
     }
 
     @Override
     public List<Customer> getAllCustomers() {
+        log.info("Running getAllCustomers() in CustomerServiceImpl...");
         return customerRepo.findAll();
     }
 
     @Override
     public Customer updateCustomerPhoneNumber(long id, String phoneNumber) {
+        log.info("Running updateCustomerPhoneNumber(" + id + ", " + phoneNumber + ") in CustomerServiceImpl...");
+        log.info("Checking customer id exists...");
         Optional<Customer> opCustomer = customerRepo.findById(id);
         if (opCustomer.isEmpty()) {
+            log.error("Invalid customer id: " + id + "!");
             throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_ID);
         } else {
+            log.info("Updating customer phone number...");
             Customer customer = opCustomer.get();
             customer.setPhoneNumber(phoneNumber);
 
+            log.info("Saving updated customer...");
             return customerRepo.save(customer);
         }
     }
 
     @Override
     public Customer updateCustomerPointsTotal(long id, int pointsTotal) {
+        log.info("Running updateCustomerPointsTotal(" + id + ", " + pointsTotal + ") in CustomerServiceImpl...");
+        log.info("Checking customer id exists...");
         Optional<Customer> opCustomer = customerRepo.findById(id);
         if (opCustomer.isEmpty()) {
+            log.error("Invalid customer id: " + id + "!");
             throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_ID);
         } else {
+            log.info("Updating customer points total...");
             Customer customer = opCustomer.get();
             customer.setTotalPoints(pointsTotal);
 
+            log.info("Saving updated customer...");
             return customerRepo.save(customer);
         }
     }
 
     @Override
     public void deleteCustomer(long id) {
+        log.info("Running deleteCustomer(" + id + ") in CustomerServiceImpl...");
+        log.info("Checking customer id exists...");
         Optional<Customer> opCustomer = customerRepo.findById(id);
         if (opCustomer.isPresent()) {
+            log.info("Deleting customer...");
             customerRepo.deleteById(id);
         } else {
-            throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_PHONE_NUMBER);
+            log.error("Invalid customer id: " + id + "!");
+            throw new CustomerException(CustomerExceptionMessages.CUSTOMER_NOT_FOUND_ID);
         }
     }
 
