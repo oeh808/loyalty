f74c289c09fe9530cc9db2d6fd679ee50d345fff
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-20 13:03:27 +0300

    Fixed typo in error message

--- src/main/java/io/brightskies/loyalty/customer/exception/CustomerExceptionMessages.java
+++ src/main/java/io/brightskies/loyalty/customer/exception/CustomerExceptionMessages.java
@@ -2,5 +2,5 @@ package io.brightskies.loyalty.customer.exception;
 
 public class CustomerExceptionMessages {
     public static String CUSTOMER_NOT_FOUND_ID = "A customer with the given id does not exist.";
-    public static String CUSTOMER_NOT_FOUND_PHONE_NUMBER = "A customer with the given id does not exist.";
+    public static String CUSTOMER_NOT_FOUND_PHONE_NUMBER = "A customer with the given phone number does not exist.";
 }
