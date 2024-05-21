2c23b096cfd70c511820afbde83552e7c3cce75e
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-21 11:27:04 +0300

    Added validation for UserDTO used for signing up and logging in

--- src/main/java/io/brightskies/loyalty/user/DTO/UserDTO.java
+++ src/main/java/io/brightskies/loyalty/user/DTO/UserDTO.java
@@ -1,5 +1,9 @@
 package io.brightskies.loyalty.user.DTO;
 
+import io.brightskies.loyalty.constants.ValidationMessages;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -8,7 +12,22 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class UserDTO {
+    @Schema(requiredProperties = {
+            ValidationMessages.NOT_BLANK_SCHEMA })
+    @NotBlank(message = "name"
+            + ValidationMessages.NOT_BLANK)
     private String name;
-    private String email ;
+
+    @Schema(requiredProperties = {
+            ValidationMessages.NOT_BLANK_SCHEMA })
+    @NotBlank(message = "email"
+            + ValidationMessages.NOT_BLANK)
+    @Email(message = ValidationMessages.INVALID_EMAIL, regexp = ValidationMessages.EMAIL_REGEX)
+    private String email;
+
+    @Schema(requiredProperties = {
+            ValidationMessages.NOT_BLANK_SCHEMA })
+    @NotBlank(message = "password"
+            + ValidationMessages.NOT_BLANK)
     private String password;
 }
