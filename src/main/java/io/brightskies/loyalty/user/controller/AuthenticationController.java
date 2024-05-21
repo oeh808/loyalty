ec769b203a2a5d65bed05ea29a92b78263f0bb7c
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-21 11:36:28 +0300

    Added swagger annotation and logging to controllers

--- src/main/java/io/brightskies/loyalty/user/controller/AuthenticationController.java
+++ src/main/java/io/brightskies/loyalty/user/controller/AuthenticationController.java
@@ -5,17 +5,23 @@ import io.brightskies.loyalty.user.DTO.LoginDTO;
 import io.brightskies.loyalty.user.DTO.UserDTO;
 import io.brightskies.loyalty.user.Entities.User;
 import io.brightskies.loyalty.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
-
+@Log4j2
 @RestController
+@Tag(name = "Authentication", description = "Controller for handling mappings for user authentication")
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins="*",allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AuthenticationController {
     @Autowired
 
@@ -26,25 +32,30 @@ public class AuthenticationController {
     @Autowired
     private final UserService userService;
 
-
+    @Operation(description = "POST endpoint for signing up a user" +
+            "\n\n Returns the user created.", summary = "Create a user")
     @PostMapping("/signup")
-    public User createUser(@RequestBody UserDTO request) {
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of UserDTO")
+    public User createUser(@Valid @RequestBody UserDTO request) {
+        log.info("Recieved: POST request to /api/v1/auth/signup");
         return userService.addUser(request);
     }
 
-
-
+    @Operation(description = "POST endpoint for logging in a user" +
+            "\n\n Returns an instance of LoginDTO which contains the user and a JWT token.", summary = "Login a user")
     @PostMapping("/login")
-    public LoginDTO login(@RequestBody UserDTO request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        //
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of UserDTO")
+    public LoginDTO login(@Valid @RequestBody UserDTO request) {
+        log.info("Recieved: POST request to /api/v1/auth/login");
+
+        authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
         final User user = userService.findByEmail(request.getEmail());
 
-        System.out.println(user.toString());
+        log.info("Logged in user: " + user.toString());
         String token = jwtUtil.generateToken(user);
 
         return new LoginDTO(user, token);
     }
 
 }
-
--- src/main/java/io/brightskies/loyalty/user/controller/Generator.java
+++ src/main/java/io/brightskies/loyalty/user/controller/Generator.java
@@ -2,19 +2,29 @@ package io.brightskies.loyalty.user.controller;
 
 import io.brightskies.loyalty.user.Entities.Role;
 import io.brightskies.loyalty.user.repository.RoleRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Log4j2
 @RestController
 @RequestMapping("api/v1/generator")
+@Tag(name = "Generator", description = "Controller for creating user roles")
 @RequiredArgsConstructor
-@CrossOrigin(origins="*",allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Generator {
     @Autowired
     private final RoleRepository roleRepository;
+
+    @Operation(description = "POST endpoint for creatin user roles" +
+            "\n\n Returns a string confirming roles were generated successfully.", summary = "Create Roles")
     @PostMapping("/roles")
     public String createRoles() {
+        log.info("Recieved: POST request to /api/v1/generator");
         roleRepository.save(new Role("ROLE_USER"));
         return "Roles created successfully";
     }
