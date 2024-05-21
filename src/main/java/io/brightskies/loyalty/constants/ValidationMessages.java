81333f28dcaea318be87ae29465fff8847fad0d0
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-21 11:36:14 +0300

    Updated validation messages to include email validation

--- src/main/java/io/brightskies/loyalty/constants/ValidationMessages.java
+++ src/main/java/io/brightskies/loyalty/constants/ValidationMessages.java
@@ -23,6 +23,10 @@ public class ValidationMessages {
     public static final String FUTURE = " must be in the future";
     public static final String FUTURE_SCHEMA = "Must be in the future";
 
+    public static final String INVALID_EMAIL = "Email must be valid";
+    public static final String EMAIL_REGEX = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
     public static final String INVALID_PHONE_NUMBER = "Phone number must be valid";
     public static final String PHONE_NUMBER_REGEX = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$";
 }
