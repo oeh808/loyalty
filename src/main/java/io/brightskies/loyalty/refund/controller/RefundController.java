090253a6199dde3e5b21c20b2a638ee754f4ae17
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-21 11:38:06 +0300

    Implemented logging to refund controller

--- src/main/java/io/brightskies/loyalty/refund/controller/RefundController.java
+++ src/main/java/io/brightskies/loyalty/refund/controller/RefundController.java
@@ -6,12 +6,14 @@ import io.brightskies.loyalty.refund.service.RefundService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @Tag(name = "Refund", description = "Controller for handling mappings for Refunds")
 @RequestMapping("/refunds")
@@ -27,6 +29,7 @@ public class RefundController {
     @PostMapping
     @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Must conform to required properties of ReFundDTO")
     public Refund createRefund(@Valid @RequestBody ReFundDTO reFundDTO) {
+        log.info("Recieved: POST request to /refunds");
         return refundService.createRefund(reFundDTO);
     }
 
