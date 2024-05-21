fbfe6013d11fe9ba86117bf6954a89f9e31fb0b2
OEH808 <omarehababdelaziz8@gmail.com>
2024-05-21 11:46:09 +0300

    Added validation to dto

--- src/main/java/io/brightskies/loyalty/refund/DTO/ProductRefundDTO.java
+++ src/main/java/io/brightskies/loyalty/refund/DTO/ProductRefundDTO.java
@@ -1,5 +1,8 @@
 package io.brightskies.loyalty.refund.DTO;
 
+import io.brightskies.loyalty.constants.ValidationMessages;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.ToString;
@@ -8,7 +11,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class ProductRefundDTO {
+    @Schema(minimum = "0")
+    @PositiveOrZero(message = "Product id"
+            + ValidationMessages.POSITIVE_OR_ZERO)
     private long productId;
+
+    @Schema(minimum = "1")
+    @PositiveOrZero(message = "quantity"
+            + ValidationMessages.POSITIVE)
     private int quantity;
 
 }
